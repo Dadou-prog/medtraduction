1a98e1b73fcdfe036608eb06644de70091c4cdf9
Dadou-prog <huulksmaash@gmail.com>
2026-05-06 13:19:49 +0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'medtraduction-cache';
+const ASSETS = ['./', './index.html', './manifest.json', './sw.js'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, clone));
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
